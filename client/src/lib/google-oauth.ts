1c18a1442c6b314270a240777e65bc54c581d85a
Rasal <rasal77473@gmail.com>
2026-01-06 16:29:17 +0530

    Update OAuth config to use only tellmamma.com domain

--- client/src/lib/google-oauth.ts
+++ client/src/lib/google-oauth.ts
@@ -3,6 +3,7 @@ export const GOOGLE_OAUTH_CONFIG = {
   clientId: "112395408252-ela7l26ctmspe0bo5cc6plhbdfb7nnoe.apps.googleusercontent.com",
   authUri: "https://accounts.google.com/o/oauth2/auth",
   tokenUri: "https://oauth2.googleapis.com/token",
+  javascriptOrigins: ["https://tellmamma.com"],
 };
 
 // Get the redirect URI for OAuth (should match server)
